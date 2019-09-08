234721940dd7891b0237609f971a21e41b2ac91b
Atai Ambus <atayambus@gmail.com>
2019-09-08 19:32:02 +0300

    recursion 12-17
    Added recursion exercises 12 through 17 to the Recursion project.

--- Recursion/Program.cs
+++ Recursion/Program.cs
@@ -4,8 +4,24 @@ namespace Recursion
 {
     class Program
     {
+
+        static string PrntArr(int[] Arr)
+        {
+            string ArrString = String.Empty;
+            ArrString += "{";
+            foreach (int num in Arr)
+            {
+                ArrString += num + ", ";
+            }
+            ArrString += "}";
+
+            return ArrString;
+        }
+
         static void Main(string[] args)
         {
+            int Result;
+            int Number;
 
             //Pick the exercise:
             while(true)
@@ -50,7 +66,7 @@ namespace Recursion
                     case("6"):
                         Console.WriteLine("This program preforms the modolus operation on a "+
                             "a given input.\n Please insert the base number.");
-                        int Number = int.Parse(Console.ReadLine());
+                        Number = int.Parse(Console.ReadLine());
                         Console.WriteLine("Now, please insert the mod: ");
                         int Mod = int.Parse(Console.ReadLine());
                         Console.WriteLine($"Result: {Number} % {Mod} = {Modulo(Number,Mod)}");
@@ -66,8 +82,8 @@ namespace Recursion
                         break;
                     case("8"):
                         Console.WriteLine("Please insert a number, and we will check if it's prime.");
-                        int Num = int.Parse(Console.ReadLine());
-                        Console.WriteLine($"{Num} is {(IsPrime(Num) ? "Prime!" : "Not Prime!")}");
+                        Number = int.Parse(Console.ReadLine());
+                        Console.WriteLine($"{Number} is {(IsPrime(Number) ? "Prime!" : "Not Prime!")}");
                         break;
                     case("9"):
                         Console.WriteLine("Please insert a number, and we will check if its digits" +
@@ -87,6 +103,76 @@ namespace Recursion
                         + "all elements in the series the sum of all elements up to it in the"
                         + "series 1 + sqrt(3) + 5 - sqrt(7) ... n / sqrt(n) ");
                         Console.WriteLine($"Result: {Exe11(int.Parse(Console.ReadLine()))}");
+                        break;
+                    case("12"):
+                        Console.WriteLine("This program calculates the sum of all intger " +
+                        "products of another integer that are smaller than an input number.\n" +
+                        "First, please insert the integer");
+                        int Base = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Now, please insert an integer maximum product. " + 
+                            "All products below it will be calculated.");
+                        int Max = int.Parse(Console.ReadLine());
+                        Console.WriteLine($"The resulting sum: {Exe12(Base, Max)}");
+                        break;
+                    case("13"):
+                        Console.WriteLine("Which Part?");
+                        string Part = Console.ReadLine();
+                        switch(Part)
+                        {
+                            case("A"):
+                                Console.WriteLine("This program calaculates elements in the series "
+                                + "where the first element is 0, the second is 1, and the rest are "
+                                + "each the sum of the squares of the two previous\n"
+                                + "Please inset the position, and we will calculate the element.");
+                                Console.WriteLine($"Your result: {Exe13A(int.Parse(Console.ReadLine()))}");
+                                break;
+                            case("B"):
+                                Console.WriteLine("This program calaculates elements in the series "
+                                + "where the first element is 0, the second is 1, and the rest are "
+                                + "each the sum of the squares of the two previous\n"
+                                + "Please insert the position, and we will calculate the sum up"
+                                + "to that position.");
+                                Console.WriteLine($"Your result: {Exe13B(int.Parse(Console.ReadLine()))}");
+                                break;
+                            default:
+                                Console.WriteLine("Part not found.");
+                                break;
+                        }
+                        break;
+                    case("14"):
+                        Console.WriteLine("This is a program which takes in an array, and sums "
+                        + "it up to a given index. In this example, we will use the array "
+                        + "[0, 6, 3, 3, 7, 8, 12, -4]\nNow, please insert the 0-based index.");
+                        Result = SumUpTo(new int[] {0, 6, 3, 3, 7, 8, 12, -4}, 
+                            int.Parse(Console.ReadLine()));
+                        Console.WriteLine($"Your result: {Result}");
+                        break;
+                    case("15"):
+                        Console.WriteLine("This is a program which takes in an array, and counts "
+                        + "how many positive numbers it has up to a given index. In this example, "
+                        + "we will use the array [0, -6, -3, 3, 7, -8, 12, 4]\n"
+                        + "Now, please insert the 0-based index.");
+                        Result = CountPositives(new int[] {0, -6, -3, 3, 7, -8, 12, 4}, 
+                            int.Parse(Console.ReadLine()));
+                        Console.WriteLine($"Your result: {Result}");
+                        break;
+                    case("16"):
+                        Console.WriteLine("This program finds position of an element in an array, "
+                        + "or returns -1 if it's not there. For this example, we will use the "
+                        + "array [0, 6, 3, -3, 7, 8, 12, -4].\n Now, please insert the element:");
+                        int Element = int.Parse(Console.ReadLine());
+                        Console.WriteLine($"Your position: "
+                        + $"{IndexOf(new int[] {0, 6, 3, -3, 7, 8, 12, -4}, Element)}");
+                        break;
+                    case("17"):
+                        Console.WriteLine("This Program checks if an array is sorted. We will test it"
+                        + "with two arrays: [0, 6, 3, -3, 7, 8, 12, -4] and its sorted version "
+                        + "([-4, -3, 0, 3, 6, 7, 8, 12]).");
+                        Console.WriteLine($"Unsorted: {IsSorted(new int[] {0, 6, 3, -3, 7, 8, 12, -4})}\n"
+                        + $"Sorted: {IsSorted(new int[] {-4, -3, 0, 3, 6, 7, 8, 12})}");
+                        break;
+                    case("18"):
+                        Console.WriteLine("");
                         break;
                     default:
                         Console.WriteLine("Exercise not found.");
@@ -250,7 +336,7 @@ namespace Recursion
         /// </returns>
         static bool Divisible(int Number, int Base)
         {
-            if( ( Base < Number && (Base < 0 || Number < 0) ) || //IF  
+            if( ( Base < Number && (Base < 0 || Number < 0) ) || 
                 ( Base > Number && (Base > 0 && Number > 0) ) )
             {
                 return false;
@@ -292,7 +378,7 @@ namespace Recursion
         }
 
 
-        //---------Exercise 9---------
+        //---------Exercise 8---------
         private static bool ParityConsistent(int Number, bool isEven)
         {
             int Digit = Number % 10; //Get current digit.
@@ -338,7 +424,13 @@ namespace Recursion
             }
         }
 
-        //---------Exercise 9---------
+        //---------Exercise 11---------
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="N">The position in the series</param>
+        /// <returns>The Nth element of the series 1 - sqrt(3) + 5 - sqrt(7) ... +n/-sqrt(n)
+        /// </returns>
         static double Exe11(int N)
         {
             if(N == 1) return 1;
@@ -346,9 +438,149 @@ namespace Recursion
             else
             {
                 int SeriesInteger = 2*N - 1;
+
                 if (N % 2 == 0) return -1 * Math.Sqrt(SeriesInteger) + Exe11(N-1);
                 else return SeriesInteger + Exe11(N-1);
             }
         }
+
+        //---------Exercise 12--------
+        /// <summary>
+        /// Calaculates the sum of all integer products of a specific intger that
+        /// are less than an input number.
+        /// </summary>
+        /// <param name="Base">The base number, that the products will be of.</param>
+        /// <param name="MaxPorduct">The maximum a product can be in the calculation.</param>
+        /// <returns>The sum of all clean products of Base that are less than MaxProduct.</returns>
+        static int Exe12(int Base, int MaxPorduct, int OriginalBase)
+        {
+            if(Base > MaxPorduct) return 0;
+            else return Base + Exe12(Base + OriginalBase, MaxPorduct, OriginalBase);
+        }
+        
+        /// <summary>
+        /// Calaculates the sum of all integer products of a specific intger that
+        /// are less than an input number.
+        /// </summary>
+        /// <param name="Base">The base number, that the products will be of.</param>
+        /// <param name="MaxPorduct">The maximum a product can be in the calculation.</param>
+        /// <returns>The sum of all clean products of Base that are less than MaxProduct.</returns>
+        static int Exe12(int Base, int MaxPorduct)
+        {
+            return Exe12(Base, MaxPorduct, Base);
+        }
+
+
+        //----------Exercise 13----------
+        //-----Part A-----
+        /// <summary>
+        /// Returns the nth number in the series where the first element is 0,
+        /// the second is 1, and the rest are each the sum of the squares of the two previous
+        /// one.
+        /// </summary>
+        /// <param name="N">The position in the series.</param>
+        /// <returns>The Nth positioned element in the series.</returns>
+        static int Exe13A(int N)
+        {
+            if(N == 1) return 0;
+            if(N == 2) return 1;
+            return (int)(Math.Pow(Exe13A(N-1), 2) + Math.Pow(Exe13A(N-2), 2));
+        }
+
+        //-----Part B-----
+        /// <summary>
+        /// Returns the sum, up to th input position, of the series where the first element is 0,
+        /// the second is 1, and the rest are each the sum of the squares of the two previous
+        /// one.
+        /// </summary>
+        /// <param name="N">The position in the series.</param>
+        /// <returns>The sum up Nth positioned element in the series.</returns>
+        static int Exe13B(int N)
+        {
+            if(N == 1) return 0;
+            else return Exe13A(N) + Exe13B(N-1);
+        }
+
+
+        //---------Exercise 14---------
+        static int SumUpTo(int[] Array, int Index)
+        {
+            if (Index == 0)
+            {
+                return Array[0];
+            }
+
+            else
+            {
+                return Array[Index] + SumUpTo(Array, Index - 1);
+            }
+        }
+
+        //---------Exercise 15--------
+        /// <summary>
+        /// Counts how many positive integers there are in an array up until a specified index.
+        /// </summary>
+        /// <param name="Array">The array to be checked.</param>
+        /// <param name="Index">The inndex to check up to. Note that as this is an index,
+        /// it must be nonnegtive and cannot be more than the length of the array.</param>
+        /// <returns>The amount of positive numbers in the array.</returns>
+        static int CountPositives(int[] Array, int Index)
+        {
+            int CurrentNumber = Array[Index];
+            
+            if(Index == 0)
+            {
+                if(CurrentNumber > 0) return 1;
+                else return 0;
+            }
+
+            else
+            {
+                if(CurrentNumber > 0) return 1 + CountPositives(Array, Index - 1);
+                else return 0 + CountPositives(Array, Index - 1);
+            }
+        }
+
+        //---------Exercise 16--------
+        private static int IndexOf(int[] Array, int Element, int Index)
+        {
+            if(Index == -1) return -1;
+            if(Array[Index] == Element) return Index;
+            else return IndexOf(Array, Element, Index - 1);
+        }
+
+
+        /// <summary>
+        /// Checks for the position of an element ina n array.
+        /// </summary>
+        /// <param name="Array">The array to search.</param>
+        /// <param name="Element">The element to search for.</param>
+        /// <returns>The position of Elment in Array, or -1 if Element is not there.</returns>
+        public static int IndexOf(int[] Array, int Element)
+        {
+            return IndexOf(Array, Element, Array.Length - 1);
+        } 
+
+        //---------Exercise 17--------
+        private static bool IsSorted(int[] Array, int Index)
+        {
+            if(Index == -1) return true;
+            if(Array[Index] < Array[Index + 1]) return false;
+            else return IsSorted(Array, Index - 1);
+        }
+
+        /// <summary>
+        /// Checks wether an integer array is sorted (lowest to highest).
+        /// </summary>
+        /// <param name="Array">The array to check.</param>
+        /// <returns>True if the array is sorted, False if not.</returns>
+        public static bool IsSorted(int[] Array)
+        {
+            return IsSorted(Array, Array.Length - 2);
+        }
+
+        
+
+
     }
 }
